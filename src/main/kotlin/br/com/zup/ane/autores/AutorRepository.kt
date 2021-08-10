add1258f72c28370015e39cb359811164eb5cc85
Greiciane93x <greiciane_araujo@hotmail.com>
2021-08-10 10:13:27 -0300

    query-value

--- src/main/kotlin/br/com/zup/ane/autores/AutorRepository.kt
+++ src/main/kotlin/br/com/zup/ane/autores/AutorRepository.kt
@@ -2,6 +2,9 @@ package br.com.zup.ane.autores
 
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
+import java.util.*
 
 @Repository
-interface AutorRepository: JpaRepository<Autor, Long>
+interface AutorRepository: JpaRepository<Autor, Long> {
+    fun findByEmail(email: String): Optional<Autor>
+}
--- src/main/kotlin/br/com/zup/ane/autores/BuscaDetalheDeAutoresController.kt
+++ src/main/kotlin/br/com/zup/ane/autores/BuscaDetalheDeAutoresController.kt
@@ -1,19 +1,32 @@
 package br.com.zup.ane.autores
 
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+import io.micronaut.validation.Validated
+import javax.transaction.Transactional
+import javax.validation.constraints.NotBlank
 
 
 @Controller("/autores")
+@Validated
 class BuscaDetalheDeAutoresController (val autorRepository: AutorRepository){
 
     @Get
-    fun lista() : MutableHttpResponse<List<DetalhesDoAutorResponse>>? {
-        val autores = autorRepository.findAll();
-        val resposta = autores.map{autor -> DetalhesDoAutorResponse(autor)}
+    @Transactional
+    fun lista(@NotBlank @QueryValue(defaultValue ="") email: String ) : HttpResponse<Any> {
 
-            return HttpResponse.ok(resposta)
+        if(email.isBlank()){
+            val autores = autorRepository.findAll()
+            val resposta = autores.map { autor -> DetalhesDoAutorResponse(autor)}
+            return HttpResponse.ok()
+        }
+        val possivelAutor = autorRepository.findByEmail(email)
+        if(possivelAutor.isEmpty){
+            return HttpResponse.notFound()
+        }
+        val autor = possivelAutor.get()
+        return HttpResponse.ok(DetalhesDoAutorResponse(autor))
     }
 }
